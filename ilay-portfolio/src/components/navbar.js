a9d54feae262c8c90e0b90b3895ed09a01ff321d
ilayG2004 <ilay2004@bu.edu>
2025-03-05 11:42:16 -0500

    Implementing carousel component to display different projects card on home page

--- ilay-portfolio/src/components/navbar.js
+++ ilay-portfolio/src/components/navbar.js
@@ -3,11 +3,12 @@ import { NavLink } from "react-router-dom";
 function navbar() {
   return (
   <div className="navbar">
-    <ul class="nav-links">
-        <li><NavLink activeStyle={{ color:'#5754a8' }} to="/">About</NavLink></li>
-        <li><NavLink activeStyle={{ color:'#5754a8' }} to="/experience">Experience </NavLink></li>
-        <li><NavLink activeStyle={{ color:'#5754a8' }} to="/projects">Projects</NavLink></li>
-        <li><NavLink activeStyle={{ color:'#5754a8' }} to="/contacts">Contacts</NavLink></li>
+    <h1> Ilay Guler </h1>
+    <ul className="nav-links">
+        <li><NavLink activeClassName="nav-active" to="/">About</NavLink></li>
+        <li><NavLink activeClassName="nav-active" to="/experience">Experience </NavLink></li>
+        <li><NavLink activeClassName="nav-active" to="/projects">Projects</NavLink></li>
+        <li><NavLink activeClassName="nav-active" to="/contacts">Contacts</NavLink></li>
     </ul>
   </div>
   );
--- ilay-portfolio/src/pages/home.js
+++ ilay-portfolio/src/pages/home.js
@@ -1,10 +1,15 @@
 import "../styles/fonts.css";
 import "../styles/homepage.css";
 import "../styles/technicalcarousel.css";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+import { Carousel } from 'antd';
 
 import TechnicalCarousel from "../components/technicalcarousel";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
+
+const { Meta } = Card;
 
 export default function Home() {
   return (
@@ -37,6 +42,34 @@ export default function Home() {
     </div>
   </section>
   
+  <hr/>
+  <section id="projects">
+    <div className="projects-container">
+    <Card style={{width:400}}>
+      <Carousel autoplay>
+        <div>
+          <img src={process.env.PUBLIC_URL + '/images/projects/sparkbyteslogo.png'}/>
+        </div>
+        <div>
+          <p>Spark! Bytes was a prototype app designed to help bring together Boston University students around something they all love -- food. Community leaders could post invites to events with descriptions, photos, tags, locations to appeal to a wider audience of BU students. Whether it was for the actual meeting, or for the take-home after the fact, the goal of this webapp was to reduce food waste by notifying BU students of these events. Attached is a frontend & backend demonstration of my team's project for Spark! Bytes.</p>
+        </div>
+      </Carousel>
+    </Card>
+    <Card style={{width:400}}>
+      <Carousel autoplay>
+        <div>
+          <img src={process.env.PUBLIC_URL + '/images/projects/sparkbyteslogo.png'}/>
+        </div>
+        <div>
+          <p>The City of Boston's budget includes an operating budget and a capital budget. The operating budget handles day-to-day expenses including public employees and services, while the capital budget is more focused on improving currently owned assets. To optimize the allocation of resources for these budgets, it is crucial to understand how Boston's money is being spent. Through collaboration with the Citywide Analytics Team, Boston's central data organization, this project analyzes how Boston spends its annual budget and how it may change over time. Myself and two other peers collaborated to visualize and make predictions on the city's +$4.7-billion budget data. </p>
+        </div>
+      </Carousel>
+    </Card>
+    </div>
+
+
+  </section>
+
   <Footer/>
   </div>
  
